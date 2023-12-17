49571183206b1c9fb4ec195bb1c89383222baea2
DmitryKnyazkov <kde.2015@yandex.ru>
2023-12-17 13:58:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dzcounter2/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.dzcounter2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
+
+class MainActivity : AppCompatActivity() {
+
+    var counter = 0
+
+    val buttonReset by lazy { findViewById<Button>(R.id.buttonReset) }
+    val remains by lazy { findViewById<TextView>(R.id.remains) }
+    val button_minus by lazy { findViewById<Button>(R.id.button_minus) }
+    val screenCounter by lazy { findViewById<TextView>(R.id.screenCounter) }
+    val button_plus by lazy { findViewById<TextView>(R.id.button_plus) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        button_minus.setOnClickListener { count(-1) }
+        button_plus.setOnClickListener { count(1) }
+        buttonReset.setOnClickListener {
+            counter = 0
+            update(counter)
+        }
+
+
+    }
+
+    fun count(value: Int) {
+        counter = counter + value
+        screenCounter.text = counter.toString()
+        update(counter)
+    }
+
+    fun update(counter: Int) {
+        if (counter == 0) {
+            remains.text = "Все места свободны"
+            button_minus.isEnabled = false
+            buttonReset.isVisible = false
+        }
+        if (counter in 1..49) {
+            remains.text = "Осталось мест: ${50 - counter}"
+            button_minus.isEnabled = true
+            buttonReset.isVisible = false}
+        else {
+            remains.text = "Пассажиров слишком много"
+            button_minus.isEnabled = true
+            buttonReset.isVisible = true
+        }
+    }
+}
